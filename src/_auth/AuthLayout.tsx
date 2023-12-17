81cd55a4061b006930eb9dd86df43d61be53877d
V415hT313 <vashu2506rkt@gmail.com>
2023-12-17 21:18:44 +0530

    added few changes

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -2,22 +2,22 @@ import { Outlet, Navigate } from 'react-router-dom';
 
 const AuthLayout = () => {
   const isAuthenticated = false;
-
+  
+  
   return (
     <>
       {isAuthenticated ? (
-        <Navigate to="/"/>
+        <Navigate to ="/" />
       ): (
         <>
           <section className="flex flex-1 justify-center items-center flex-col py-10">
             <Outlet/>
           </section>
 
-          <img
+          <img 
             src="assets/images/side-img.svg"
             alt="logo"
             className="hidden x1:block h-screen w-1/2 object-cover bg-no-repeat"
-          
           />
         </>
       )}
@@ -25,8 +25,4 @@ const AuthLayout = () => {
   )
 }
  
-  
-  
-  
-
 export default AuthLayout
