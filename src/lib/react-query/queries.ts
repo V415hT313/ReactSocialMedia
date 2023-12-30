7b13ff417e4ae601adc2e6154cad1bd65951c3f6
V415hT313 <vashu2506rkt@gmail.com>
2023-12-30 20:39:50 +0530

    ---

--- src/lib/react-query/queries.ts
+++ src/lib/react-query/queries.ts
@@ -2,7 +2,7 @@ import {
   useQuery,
   useMutation,
   useQueryClient,
-  useInfiniteQuery,
+ 
 } from "@tanstack/react-query";
 
 import { QUERY_KEYS } from "@/lib/react-query/queryKeys";
@@ -21,7 +21,7 @@ import {
   getUserById,
   updateUser,
   getRecentPosts,
-  getInfinitePosts,
+ 
   searchPosts,
   savePost,
   deleteSavedPost,
