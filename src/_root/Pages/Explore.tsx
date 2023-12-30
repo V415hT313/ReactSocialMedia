3f4fb15a124df52c82939dc821ceb9dd171e0dfc
V415hT313 <vashu2506rkt@gmail.com>
2023-12-30 20:32:49 +0530

    ---

--- src/_root/Pages/Explore.tsx
+++ src/_root/Pages/Explore.tsx
@@ -1,9 +1,19 @@
-import { useState } from 'react'
-import { Input } from '@/components/ui/input'
+import { useState } from "react";
+
+import { Input } from "@/components/ui";
+
+export type SearchResultProps = {
+  isSearchFetching: boolean;
+  searchedPosts: any;
+};
+
 
 const Explore = () => {
-  const [searchValue, setSearchValue] = useState('')
+  
+  
 
+  const [searchValue, setSearchValue] = useState("");
+  
   
 
   
@@ -19,18 +29,22 @@ const Explore = () => {
             alt="search"
           />
           <Input
-            type="text" 
+            type="text"
             placeholder="Search"
             className="explore-search"
             value={searchValue}
-            onChange={(e) => setSearchValue(e.target.value)}
+            onChange={(e) => {
+              const { value } = e.target;
+              setSearchValue(value);
+            }}
           />
         </div>
       </div>
 
       <div className="flex-between w-full max-w-5xl mt-16 mb-7">
         <h3 className="body-bold md:h3-bold">Popular Today</h3>
-        <div className="flex-center gap-3 bg-dark-3 rounded-x1 px-4 py-2 cursor-pointer">
+
+        <div className="flex-center gap-3 bg-dark-3 rounded-xl px-4 py-2 cursor-pointer">
           <p className="small-medium md:base-medium text-light-2">All</p>
           <img
             src="/assets/icons/filter.svg"
@@ -40,9 +54,11 @@ const Explore = () => {
           />
         </div>
       </div>
-    </div>  
-      
-  )
-}
 
-export default Explore
+      
+      
+    </div>
+  );
+};
+
+export default Explore;
